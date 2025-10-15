bc8003edca37fdfae8e6fa1e422b85d244ab499d
QTL1606 <tailoi1606@gmail.com>
2025-10-15 07:44:33 +0700

    checkconnect

--- src/app.js
+++ src/app.js
@@ -2,13 +2,19 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const compression = require('compression');
+const coutConnect = require('./helpers/check.connect');
 const app = express()
+
+
 
 //init middlexwares
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(compression());
+app.use(compression()); 
 //init db
+require('./db/init.mongodb')
+
+
 
 //init route
 app.get('/', (req,res,next) =>{
--- src/db/init.mongodb.js
+++ src/db/init.mongodb.js
@@ -3,4 +3,33 @@ const mongoose = require('mongoose');
 
 const connectString = `mongodb://localhost:27017/`;
 
-mongoose.connect(connectString).then(() => console.log('Connected to MongoDB')).catch(err => console.log('Error connecting to MongoDB', err));
+const checkCount  = require('../helpers/check.connect')
+
+
+// Singleton Design dùng để gọi chỉ 1 lần  không làm tốn tài nguyên 
+class Database{
+
+    constructor(){
+        this.connect()
+    }
+
+    connect(type = 'mongodb'){
+        if ( 1 == 1){ //nếu ko cần debug thì chỉ cần chuyển 1 == 0 là được 
+            mongoose.set('debug', true) //còn này thì là debug mod
+            mongoose.set('debug',{color : true} ) //thêm màu sắc cho debug
+        }
+        mongoose.connect(connectString).then(_=> {console.log(`Connected to MongoDB  `),  checkCount.countConnect()})
+        .catch(err => console.log('Error connecting to MongoDB', err))
+    }
+
+    static getInstance(){
+        if (!Database.instance){
+            Database.instance = new Database()
+        }
+        return Database.instance
+    }
+     
+}
+const instanceMongoDb = Database.getInstance()
+
+module.exports = mongoose
